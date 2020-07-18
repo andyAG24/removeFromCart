0e6841157c20c04ca0e955f2f886bf662b032317
andyAG24 <andy.ag.24@gmail.com>
2020-07-19 01:06:19 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
+'use strict';
+
 const config = {
     siteUrl: '' // URL сайта
 };
 
 function removeItem(skuId) {
     var xhr = new XMLHttpRequest();
-    var requestUrl = `${config.siteUrl}/.wf_graphql/apollo`
-    xhr.open("POST", requestUrl, false);
+    var requestUrl = `${config.siteUrl}/.wf_graphql/apollo`;
+    xhr.open('POST', requestUrl, false);
     var requestBody = ([
         {
-            operationName: "AddToCart",
+            operationName: 'AddToCart',
             variables: {
                 skuId: skuId,
                 count: 0
@@ -20,6 +22,6 @@ function removeItem(skuId) {
 
 var removeFromCartButton = $('.removefromcartbutton.w-button');
 $(removeFromCartButton).each(() => {
-    let skuId = $(this).parent().attr("data-commerce-sku-id");
+    let skuId = $(this).parent().attr('data-commerce-sku-id');
     removeItem(skuId);
 });
